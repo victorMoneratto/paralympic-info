9cc804d4af3d44be3f08fd602c2514bac5dfe880
Victor Moneratto <victor.moneratto@gmail.com>
2016-06-14 23:55:48 -0300

    Revamp the samples into reusable templates

--- src/main/java/info/Main.java
+++ src/main/java/info/Main.java
@@ -1,19 +1,21 @@
 package info;
 
 import info.view.MainController;
-import info.view.SampleController;
 import io.datafx.controller.flow.Flow;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import javax.inject.Inject;
 import java.sql.SQLException;
 
 public class Main extends Application {
 
+    @Inject
+    DataAccess data;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-        new Flow(SampleController.class).startInStage(primaryStage);
-//        new Flow(MainController.class).startInStage(primaryStage);
+        new Flow(MainController.class).startInStage(primaryStage);
     }
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
--- src/main/java/info/view/MainController.java
+++ src/main/java/info/view/MainController.java
@@ -1,7 +1,24 @@
 package info.view;
 
 import io.datafx.controller.FXMLController;
+import io.datafx.controller.flow.action.LinkAction;
+import io.datafx.core.concurrent.ProcessChain;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import javax.annotation.PostConstruct;
 
 @FXMLController(value = "main.fxml", title = "Paralimpíadas")
 public class MainController  {
+
+    @FXML
+    @LinkAction(LocalTableController.class)
+    private Button localButton;
+
+    @PostConstruct
+    public void init() {
+        new ProcessChain()
+                .addRunnableInPlatformThread(() -> localButton.getScene().getWindow().sizeToScene())
+                .run();
+    }
 }
